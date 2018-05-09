7420e848f55c2681d8f0879ea20dad81481e5d0d
Sampath Tharanga <sampaththaranga13@gmail.com>
2018-05-09 09:17:02 +0530

    Database Model created

--- ASP.NetMVC_CRUD/Controllers/EmployeeController.cs
+++ ASP.NetMVC_CRUD/Controllers/EmployeeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ASP.NetMVC_CRUD.Models;
 
 namespace ASP.NetMVC_CRUD.Controllers
 {
@@ -12,6 +13,15 @@ namespace ASP.NetMVC_CRUD.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult GetData()
+        {
+            using (DBModel db=new DBModel())
+            {
+                List<Employee> empList = db.Employees.ToList<Employee>();
+                return Json(new { data = empList }, JsonRequestBehavior.AllowGet);
+            }
         }
     }
 }
