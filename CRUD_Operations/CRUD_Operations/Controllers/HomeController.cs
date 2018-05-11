f356470f1a1829ac1bc82e458c4cf03ac58f8259
Sampath Tharanga <sampaththaranga13@gmail.com>
2018-05-11 14:25:33 +0530

    New employee save action complete

--- CRUD_Operations/CRUD_Operations/Controllers/HomeController.cs
+++ CRUD_Operations/CRUD_Operations/Controllers/HomeController.cs
@@ -23,5 +23,54 @@ namespace CRUD_Operations.Controllers
                 return Json(new { data = employees }, JsonRequestBehavior.AllowGet);
             }
         }
+
+        [HttpGet]
+        public ActionResult Save(int id)
+        {
+            using (MyDatabaseEntities dc = new MyDatabaseEntities())
+            {
+                var v = dc.EmployeesTables.Where(a => a.EmployeeID == id).FirstOrDefault();
+                return View();
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Save(EmployeesTable emp)
+        {
+            bool status = false;
+            if (ModelState.IsValid)
+            {
+                using (MyDatabaseEntities dc = new MyDatabaseEntities())
+                {
+                    if (emp.EmployeeID > 0)
+                    {
+                        //Save
+                        var v = dc.EmployeesTables.Where(a => a.EmployeeID == emp.EmployeeID).FirstOrDefault();
+                        if (v != null)
+                        {
+                            v.FirstName = emp.FirstName;
+                            v.LastName = emp.LastName;
+                            v.EmailID = emp.EmailID;
+                            v.City = emp.City;
+                            v.Country = emp.Country;
+                        }
+                    }
+                    else
+                    {
+                        //Edit
+                        dc.EmployeesTables.Add(emp);
+                    }
+                    dc.SaveChanges();
+                    status = true;
+                }
+            }
+            return new JsonResult { Data = new { status = status } };
+        }
+
+        [HttpGet]
+        public ActionResult Delete (int id)
+        {
+
+        }
     }
 }
